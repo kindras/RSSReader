9e5ed1cf982b8d1522ccaa11ded1ab9cba18d94a
kindras <barrier.anthony@gmail.com>
2015-03-23 13:50:55 +0100

    Correction des vendors + Ajout d'un systeme de credentials décentralisé

---
+++ src/Credentials/Credentials.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barrier
+ * Date: 23/03/15
+ * Time: 13:42
+ */
+
+class Credentials {
+
+    public static $user = "root";
+
+    public static $pass = "root";
+
+    public static $dbName = "RSSReader";
+
+    public static $dbUrl = "localhost";
+
+}
